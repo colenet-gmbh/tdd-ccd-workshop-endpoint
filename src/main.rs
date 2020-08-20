33624e61bfc3c7c59a7afb5f6e917e60e954b670
Ralf Waldvogel <ralf.waldvogel@gmail.com>
2020-08-20 11:47:29 +0200

    github users

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,59 @@ pub struct MockUser {
     pub followers: Vec<Follower>,
 }
 
+
+#[derive(Deserialize, Serialize, Clone)]
+pub struct GithubUser {
+    login: String,
+    id: i64,
+    node_id: String,
+    avatar_url: String,
+    gravatar_id: String,
+    url: String,
+    html_url: String,
+    followers_url: String,
+    following_url: String,
+    gists_url: String,
+    starred_url: String,
+    subscriptions_url: String,
+    organizations_url: String,
+    repos_url: String,
+    events_url: String,
+    received_events_url: String,
+    site_admin: bool,
+    name: String,
+    company: String,
+    blog: String,
+    location: String,
+    email: Option<String>,
+    hireable: Option<String>,
+    bio: Option<String>,
+    twitter_username: Option<String>,
+    public_repos: u32,
+    public_gists: u32,
+    followers: u32,
+    following: u32,
+    created_at: String,
+    updated_at: String
+}
+
+
 fn find_user(user_name: String) -> std::result::Result<Option<MockUser>, serde_json::error::Error>
 {
     let users = include_str!("users.json");
     let users: Vec<MockUser> = serde_json::from_str(users)?;
     let user: Option<MockUser> = users.into_iter()
                 .find(|user| user.login.eq(&user_name.to_string()));
+
+    Ok(user)
+}
+
+fn find_github_user(login: String) -> std::result::Result<Option<GithubUser>, serde_json::error::Error>
+{
+    let users = include_str!("github.json");
+    let users: Vec<GithubUser> = serde_json::from_str(users)?;
+    let user: Option<GithubUser> = users.into_iter()
+                .find(|user| user.login.eq(&login.to_string()));
 
     Ok(user)
 }
@@ -39,10 +86,36 @@ mod test {
             assert_eq!("Fehler", "");
         }
     }
+
+    #[test]
+    fn test_find_github_user() {
+        let actual = find_github_user(String::from("maschonber"));
+        if let Ok(Some(user)) = actual {
+            assert_eq!(user.name, "Martin Schönberger")
+        } else {
+            assert_eq!("Fehler", "");
+        }
+    }
 }
 
 #[get("/users/{user_name}")]
 async fn get_mock_user(user_name: web::Path<String>) -> Result<web::Json<MockUser>> {
+    use std::time::Duration;
+    
+    let mut rng: ThreadRng = rand::thread_rng();
+    let delay: u64 = rng.gen_range(300, 1200);    
+    futures_timer::Delay::new(Duration::from_millis(delay)).await;
+
+    let user = find_user(user_name.to_string())?;
+
+    match user {
+        Some(user) => Ok(web::Json(user.clone())),
+        None => Err(ErrorNotFound("user does not exist"))
+    }
+}
+
+#[get("/github/{user_name}")]
+async fn get_mock_github_user(user_name: web::Path<String>) -> Result<web::Json<MockUser>> {
     use std::time::Duration;
     
     let mut rng: ThreadRng = rand::thread_rng();
